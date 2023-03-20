666c07ce709f90fa80d79a7697aea2ba5d215d82
CGermanSoto <c.germansoto@gmail.com>
2023-03-20 08:48:22 -0300

    First commit

---
+++ src/main/java/IGU/Principal.java
@@ -0,0 +1,7 @@
+package IGU;
+
+import javax.swing.*;
+
+public class Principal {
+    private JPanel panel1;
+}
---
+++ src/main/java/org/automovil/Main.java
@@ -0,0 +1,6 @@
+package org.automovil;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
